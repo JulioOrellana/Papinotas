9b3f25f1c3651e8dd32cb97d3e9c4b1b8807ac73
Julio Orellana <julioa.orellana@gmail.com>
2018-10-20 20:35:03 -0300

    Added input from console and help messages

--- Algoritmo/algoritmo.rb
+++ Algoritmo/algoritmo.rb
@@ -6,13 +6,17 @@
 # end
 
 # Array of balanced strings, can be 0 to n
-balancedStrings = ['(()[]())', '((([])))','((((((())]',]
+# balancedStrings = ['(()[]())', '((([])))','((((((())]',]
 
-if balancedStrings.empty?
-    puts "Sin elementos"
-    abort
-end
+# if balancedStrings.empty?
+#     puts "Sin elementos"
+#     abort
+# end
 
+
+# @params char (character)
+# @returns char
+# Get the opposite char of input. If value is not listed, then default value is empty string.
 def getOpposite(char)
     case char
     when '['
@@ -37,24 +41,51 @@ end
 # ( = 40 , ) = 41
 # { = 123 , } = 125
 
-balancedStrings.each do | bString |
-    if(bString.length % 2 != 0)
-        puts "#{bString} -> No cumple"
+
+# @params input (String)
+# @return nothing 
+# Shows a message in console if value is correct or not
+def balancedString(input)
+
+    # If input is empty, abort execution
+    if input.empty?
+        puts "Incorrecto"
+        abort
+    end
+
+    # If input is not even, means the string is not balanced
+    if(input.length % 2 != 0)
+        puts "No cumple #{input.length}"
+
+    # If input is even
     else
-        bStringChar = bString.split('')
+
+        # Make an array of chars
+        bStringChar = input.split('')
         
+        # Default value
         iSCorrect = true
         
+        # Iterate over the half of the array length
         (bStringChar.length/2).times do | number |
+
+            # Evaluate the values of the extremes up to the inside
+            # First we get the opposite from current position and then we evaluate if it is equal to the other side.
             if getOpposite(bStringChar[number]) != bStringChar[bStringChar.length - number - 1]
                 iSCorrect = false
             end
         end
-    end
-    
-    if(iSCorrect)
-        puts "#{bString} -> es Correcto"
-    else
-        puts "#{bString} -> es Incorrecto"
+
+        if(iSCorrect)
+            puts "es Correcto"
+        else
+            puts "es Incorrecto"
+        end
     end
 end
+
+# Call value from console input
+balancedString = $stdin.readline.strip.to_s
+
+# Function to return noting but a message 
+balancedString(balancedString)
